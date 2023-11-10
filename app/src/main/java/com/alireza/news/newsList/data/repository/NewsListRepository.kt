7adcd1aad0abb0931977603aec731b99e4ab1915
Alireza Noori <alireza.nr1378@gmail.com>
2023-11-10 04:43:42 -0800

    Revert "Chore/asad/refactor structure"

--- app/src/main/java/com/alireza/news/newsList/data/repository/NewsListRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.alireza.news.newsList.data.repository
-
-import com.alireza.news.newsList.data.dataSource.local.entity.NewsArticleEntity
-import kotlinx.coroutines.flow.Flow
-
-interface NewsListRepository {
-
-    suspend fun fetchNewsList(): Flow<List<NewsArticleEntity>>
-}
