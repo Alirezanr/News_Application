9e12ca80deb8fe9a9ad1a8fea5cc6284c8797925
Alireza Noori <alireza.nr1378@gmail.com>
2023-12-15 14:28:14 +0330

    Provide dispatchers through Hilt in order to have easier testing later.

--- app/src/main/java/com/alireza/news_app/feature_news/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/alireza/news_app/feature_news/data/repository/NewsRepositoryImpl.kt
@@ -2,17 +2,17 @@ package com.alireza.news_app.feature_news.data.repository
 
 import com.alireza.news_app.core.domain.ResultWrapper
 import com.alireza.news_app.core.data.safeApiCall
+import com.alireza.news_app.di.IoDispatcher
 import com.alireza.news_app.feature_news.data.remote.NewsApi
 import com.alireza.news_app.feature_news.data.remote.dto.toDomain
 import com.alireza.news_app.feature_news.domain.model.News
 import com.alireza.news_app.feature_news.domain.repository.NewsRepository
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 class NewsRepositoryImpl @Inject constructor(
     private val service: NewsApi,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+    @IoDispatcher private val dispatcher: CoroutineDispatcher,
 ) : NewsRepository {
 
     override suspend fun getTopHeadlines(): ResultWrapper<News> = safeApiCall(dispatcher) {
